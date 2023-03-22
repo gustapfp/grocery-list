e461cdf8b48fbffaac909de1c204a2c75cff61a8
Gustavo Figueiredo <gustavopfp.trab@gmail.com>
2023-03-22 11:24:04 -0300

    get validations

--- routes/item.py
+++ routes/item.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException
 from config.db import DB_grocery
 from models.item import Item
 from schemas.schemas import itemEntity, itemsEntity
 from bson import ObjectId
-from pymongo import DESCENDING
+
 
 item = APIRouter(
     tags=['Items'], 
@@ -32,6 +32,9 @@ async def get_all_items():
     Returns:
         _type_: list of items 
     """
+    items = list(DB_grocery.item.find())
+    if len(items) == 0:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="This list is empity! Try add some items in the list.")
     return itemsEntity(DB_grocery.item.find())
 
 @item.get('/{id}', status_code=status.HTTP_200_OK)
@@ -42,7 +45,11 @@ async def get_item(id):
     Returns:
         _type_: _description_
     """
-    return itemEntity(DB_grocery.item.find_one({'_id': ObjectId(id)}))
+    selected_item = DB_grocery.item.find_one({'_id': ObjectId(id)})
+    
+    if not selected_item:
+         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The list elemento of id: {id}, doesn't exist!") 
+    return itemEntity(selected_item)
 
 @item.put('/{id}', status_code=status.HTTP_200_OK)
 async def update_item(id, item: Item):
